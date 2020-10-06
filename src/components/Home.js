c6881747db72ce4568fce22cb65f511cdbef6303
smoumita19 <smoumita19@yahoo.com>
2020-10-06 18:38:05 +0530

    Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -9,6 +10,57 @@ function Home() {
         src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Xiaomi/Redmi_9A/Flash_Sales/Sale_Today/V242308626_IN_WLME_Redmi_G3L_Tallhero_1500x600._CB406706865_.jpg"
         alt=""
       ></img>
+
+      {/*Product id, title, price, image*/}
+      {/*Product*/}
+      <div className="home__row">
+        <Product
+          id="123"
+          title="The Call of Cthulhu"
+          price={455}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2gi4w35L._SX329_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123"
+          title="The Call of Cthulhu"
+          price={455}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2gi4w35L._SX329_BO1,204,203,200_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="123"
+          title="The Call of Cthulhu"
+          price={455}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2gi4w35L._SX329_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123"
+          title="The Call of Cthulhu"
+          price={455}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2gi4w35L._SX329_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123"
+          title="The Call of Cthulhu"
+          price={455}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2gi4w35L._SX329_BO1,204,203,200_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="123"
+          title="The Call of Cthulhu"
+          price={455}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51R2gi4w35L._SX329_BO1,204,203,200_.jpg"
+        />
+      </div>
     </div>
   );
 }
